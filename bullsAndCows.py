127c37c538373623ab801bb11040428c8cbd6e2e
Arsenei Abraamyan <33315761+UlvacMoscow@users.noreply.github.com>
2018-06-05 18:13:46 +0300

    Add files via upload
    first commit

---
+++ bullsAndCows.py
@@ -0,0 +1,28 @@
+import random
+
+
+def get_all_answers():
+    ans = []
+    for i in range(10000):
+        tmp = str(i).zfill(4)
+        # print(tmp)
+        # variant 1 set
+    #     if len(set(map(int, tmp))) == 4:
+    #         ans.append(list(map(int, tmp)))
+    # print(ans)
+        # variant 2 generator-lists
+        lst = ['x' for num in tmp if tmp.count(num) == 1]
+        if len(lst) == 4:
+            ans.append(list(map(int, tmp)))
+    print(ans)
+
+
+def input_number():
+    pass
+
+
+answers = 0
+player = 0
+enemy = 0
+
+get_all_answers()
