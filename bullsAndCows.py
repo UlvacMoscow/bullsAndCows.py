1cd8ca7056e03a58855c9d41b602d9ceb83bd464
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-06-25 18:54:48 +0300

    add def check, del_bad_answers

--- bullsAndCows.py
+++ bullsAndCows.py
@@ -14,16 +14,50 @@ def get_all_answers():
         lst = ['x' for num in tmp if tmp.count(num) == 1]
         if len(lst) == 4:
             ans.append(list(map(int, tmp)))
-    print(ans)
+    return ans
+
+
+def get_one_answer(ans):
+    num = random.choice(ans)
+    return num
 
 
 def input_number():
     while True:
-        nams = input('введите 4 непоследующих числа')
+        nums = input('введите 4 неповторяющихся числа')
+        if len(nums) != 4 or not nums.isdigit():
+            continue
+        nums = list(map(int, nums))
+        if len(set(nums)) == 4:
+            break
+    return nums
 
 
-answers = 0
-player = 0
-enemy = 0
+def check(nums, true_nums):
+    bulls, cows = 0, 0
+    for i, num in enumerate(nums):
+        if num in true_nums:
+            if nums[i] == true_nums[i]:
+                bulls += 1
+            else:
+                cows += 1
+    return bulls, cows
 
-get_all_answers()
+
+def del_bad_answers(ans, enemy_try, bull, cow):
+    for num in ans[:]:
+        temp_bull, temp_cow = check(num, enemy_try)
+        if temp_bull != bull or temp_cow != cow:
+            ans.remove(num)
+    return ans
+
+
+
+answers = get_all_answers()
+player = input_number()
+enemy = get_one_answer(answers)
+
+print(enemy)
+
+
+
